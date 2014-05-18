fcc194eea34eaf02a4d0ea01e91a31d27905be94
Guilherme Livi <gui_livi@hotmail.com>
2014-05-18 14:27:44 -0300

    Add image with paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
+
+Paperclip.options[:command_path] = "/usr/local/bin/"
 Rails.application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -37,4 +40,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
 end
